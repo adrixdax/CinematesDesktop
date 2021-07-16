c554d629ac853d8acf38707fdb658225cf0098ee
adrixdax <47791835+adrixdax@users.noreply.github.com>
2021-07-16 12:12:23 +0200

    Date Format per date BigQuery Esempio di cambio scena JavaFx Piccolo test sui datasets per capire le BigQuery

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,5 +1,9 @@
 package sample;
 
+import com.google.api.gax.paging.Page;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.bigquery.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -9,7 +13,15 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class Controller {
@@ -27,10 +39,53 @@ public class Controller {
                 stage = (Stage) (((Node) mouseEvent.getSource()).getScene().getWindow());
                 stage.setScene(new Scene(root, 1280, 720));
                 stage.show();
+                try {
+                    testBigQuery();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
         }
     }
+
+    private String dateToBigQueryFormat(long date){
+        return new SimpleDateFormat("yyyyMMdd").format(new Date(date));
+    }
+
+    private String dateToBigQueryFormat(Date date){
+        return new SimpleDateFormat("yyyyMMdd").format(date);
+    }
+
+    public void testBigQuery() throws InterruptedException, FileNotFoundException {
+        System.out.println(dateToBigQueryFormat(new Date(System.currentTimeMillis())));
+        System.out.println(dateToBigQueryFormat(System.currentTimeMillis()));
+        String projectId = "ingsw2021";
+        File credentialsPath = new File(Objects.requireNonNull(getClass().getResource("../ingsw2021-bf069d24a538.json")).getPath());
+        System.out.println(credentialsPath.getAbsolutePath());
+        // Load credentials from JSON key file. If you can't set the GOOGLE_APPLICATION_CREDENTIALS
+        // environment variable, you can explicitly load the credentials file to construct the
+        // credentials.
+        GoogleCredentials credentials = null;
+        try (FileInputStream serviceAccountStream = new FileInputStream(credentialsPath)) {
+            credentials = ServiceAccountCredentials.fromStream(serviceAccountStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BigQuery bigquery =
+                BigQueryOptions.newBuilder()
+                        .setCredentials(credentials)
+                        .setProjectId(projectId)
+                        .build()
+                        .getService();
+        // Use the client.
+        System.out.println("Datasets:");
+        for (Dataset dataset : bigquery.listDatasets().iterateAll()) {
+            System.out.printf("%s%n", dataset.getDatasetId().getDataset());
+        }
+    }
+
 }
